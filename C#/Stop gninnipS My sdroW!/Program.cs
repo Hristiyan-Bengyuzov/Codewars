73eee20b1d90937590941b6da3313384361f2605
Hris <xrisko06@abv.bg>
2023-09-26 22:28:06 +0300

    added kata

---
+++ C#/Stop gninnipS My sdroW!/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Kata
+{
+    public static string SpinWords(string sentence)
+    {
+        string[] words = sentence.Split();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length >= 5)
+                words[i] = new string(words[i].Reverse().ToArray());
+        }
+
+        return string.Join(' ', words);
+    }
+}
