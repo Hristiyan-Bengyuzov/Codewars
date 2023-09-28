72c3cb5744cb7ec365e4beee430df1348f72a9f9
Hris <xrisko06@abv.bg>
2023-09-28 23:24:53 +0300

    added kata

---
+++ C#/N-th Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Fibonacci
+{
+    public int NthFib(int n)
+    {
+        int a = 0;
+        int b = 1;
+        int c = 0;
+
+        if (n == 1) return a;
+        if (n == 2) return b;
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c;
+    }
+}
