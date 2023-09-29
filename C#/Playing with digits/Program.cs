685c41c94e569549ebfd47b0d77981b3c86062eb
Hris <xrisko06@abv.bg>
2023-09-29 21:18:22 +0300

    added kata

---
+++ C#/Playing with digits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class DigPow
+{
+	public static long digPow(int n, int p)
+	{
+		int originalValue = n;
+		int rev = 0;
+		int sum = 0;
+
+		while (n != 0) // reversing the number
+		{
+			rev = rev * 10 + n % 10;
+			n /= 10;
+		}
+
+		while (rev != 0) // calculating the sum
+		{
+			int currentDigit = rev % 10;
+			sum += (int)Math.Pow(currentDigit, p++);
+			rev /= 10;
+		}
+
+		return sum % originalValue == 0 ? sum / originalValue : -1;
+	}
+}
