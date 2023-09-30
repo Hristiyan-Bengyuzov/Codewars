08a46947a3584edeb85d71f5697140bf84682610
Hris <xrisko06@abv.bg>
2023-09-30 21:57:31 +0300

    added kata

---
+++ C#/Unique In Order/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+public static class Kata
+{
+	public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+	{
+		T previous = default(T);
+		foreach (T current in iterable)
+		{
+			if (!current.Equals(previous))
+			{
+				previous = current;
+				yield return current;
+			}
+		}
+	}
+}
