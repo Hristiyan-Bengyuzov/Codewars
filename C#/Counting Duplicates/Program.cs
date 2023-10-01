60678c854022c912156b891a42b5f62446f5de81
Hris <xrisko06@abv.bg>
2023-10-01 22:43:08 +0300

    added kata

---
+++ C#/Counting Duplicates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int DuplicateCount(string str)
+    {
+        str = str.ToLower(); // case-insensitive
+        var charsByOccurences = new Dictionary<char, int>();
+
+        foreach (var item in str)
+        {
+            if (!charsByOccurences.ContainsKey(item))
+                charsByOccurences.Add(item, 0);
+            charsByOccurences[item]++;
+        }
+
+        return charsByOccurences.Count(c => c.Value > 1); // number of duplicates
+    }
+}
