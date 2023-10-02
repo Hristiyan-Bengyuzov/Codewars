26e50dee5a90fb19182b58147f7a096b8e08b48a
Hris <xrisko06@abv.bg>
2023-10-02 23:07:13 +0300

    added kata

---
+++ C#/Take a Ten Minutes Walk/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Kata
+{
+    public static bool IsValidWalk(string[] walk)
+    {
+        int defPlayerRow = 0;
+        int defPlayerCol = 0;
+        int finalRow = 0;
+        int finalCol = 0;
+
+        foreach (var direction in walk)
+        {
+            switch (direction)
+            {
+                case "n": finalRow--; break;
+                case "s": finalRow++; break;
+                case "w": finalCol--; break;
+                case "e": finalCol++; break;
+            }
+        }
+
+        return defPlayerRow == finalRow && defPlayerCol == finalCol && walk.Length == 10;
+    }
+}
