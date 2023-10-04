ab743cebb458744b99e56d56ee444c6a41193a91
Hris <xrisko06@abv.bg>
2023-10-04 18:41:10 +0300

    added kata

---
+++ C#/Directions Reduction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class DirReduction
+{
+    public static string[] dirReduc(String[] arr)
+    {
+        var opposite = new Dictionary<string, string>
+        {
+            {"NORTH", "SOUTH"},
+            {"EAST", "WEST"},
+            {"SOUTH", "NORTH"},
+            {"WEST", "EAST"}
+        };
+
+        var stack = new Stack<string>();
+
+        foreach (var dir in arr)
+        {
+            if (stack.Any() && opposite[dir] == stack.Peek())
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(dir);
+            }
+        }
+
+        return stack.Reverse().ToArray();
+    }
+}
