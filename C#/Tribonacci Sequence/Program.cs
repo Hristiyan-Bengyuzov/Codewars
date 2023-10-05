8ea51bd77a2517ef08a235f8c3a8ea5904907264
Hris <xrisko06@abv.bg>
2023-10-05 23:02:19 +0300

    added kata

---
+++ C#/Tribonacci Sequence/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Xbonacci
+{
+    public double[] Tribonacci(double[] signature, int n)
+    {
+        List<double> tribonacciNums = new List<double>(signature);
+
+        for (int i = 3; i < n; i++)
+            tribonacciNums.Add(tribonacciNums[i - 3] + tribonacciNums[i - 2] + tribonacciNums[i - 1]);
+
+        return tribonacciNums.Take(n).ToArray();
+    }
+}
