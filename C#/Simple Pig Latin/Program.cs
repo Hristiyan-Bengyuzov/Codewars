f3b82b18b5eae9f7ac7241b9d7efc15c9a3590ea
Hris <xrisko06@abv.bg>
2023-10-06 22:06:20 +0300

    added kata

---
+++ C#/Simple Pig Latin/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Kata
+{
+	public static string PigIt(string str)
+	{
+		string[] arr = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+		for (int i = 0; i < arr.Length; i++)
+		{
+			// variables
+			string currentWord = arr[i];
+			char firstChar = currentWord[0];
+
+			if (char.IsPunctuation(firstChar)) continue; //  leave punctuation marks untouched
+
+			currentWord = currentWord.Substring(1); // removing first character
+			currentWord += $"{firstChar}ay"; // appending first character and "ay"
+
+			arr[i] = currentWord;
+		}
+
+		return string.Join(' ', arr);
+	}
+}
