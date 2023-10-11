25b16b84356a0cce82a23c9c6c3e8a7bc38091e9
Hris <xrisko06@abv.bg>
2023-10-11 22:23:23 +0300

    added kata

---
+++ C#/Maximum subarray sum/Program.cs
@@ -0,0 +1,21 @@
+﻿public static class Kata
+{
+    // Kadane’s algorithm
+    public static int MaxSequence(int[] arr)
+    {
+        int maxSoFar = int.MinValue, maxEndingHere = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            maxEndingHere += arr[i];
+
+            if (maxSoFar < maxEndingHere)
+                maxSoFar = maxEndingHere;
+
+            if (maxEndingHere < 0)
+                maxEndingHere = 0;
+        }
+
+        return maxSoFar == int.MinValue ? 0 : maxSoFar;
+    }
+}
