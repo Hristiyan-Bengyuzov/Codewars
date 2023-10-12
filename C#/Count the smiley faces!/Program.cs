59c27d0203825da2b561a4b497e198049b1bd6ea
Hris <xrisko06@abv.bg>
2023-10-12 23:21:47 +0300

    added kata

---
+++ C#/Count the smiley faces!/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+    public static int CountSmileys(string[] smileys) => smileys.Count(s => new Regex(@"^[:;][-~]?[)D]$").IsMatch(s));
+}
