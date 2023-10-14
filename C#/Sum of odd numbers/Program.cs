49de4641e9d7d38a2525b7e586a7aefa58e370bb
Hris <xrisko06@abv.bg>
2023-10-14 22:20:24 +0300

    added kata

---
+++ C#/Sum of odd numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class Kata
+{
+	public static long RowSumOddNumbers(long n)
+	{
+		long start = n * n - (n - 1);
+		long end = start + 2 * n;
+		long sum = 0;
+
+		for (long i = start; i < end; i += 2)
+		{
+			sum += i;
+		}
+
+		return sum;
+	}
+}
